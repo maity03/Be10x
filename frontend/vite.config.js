f2ed358b0eb84aa65b36039b28a275a677b7c6b4
maity03 <biman123maity@gmail.com>
2025-07-10 16:16:36 +0530

    link added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const backendPort = 5000;
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
     proxy: {
-      "/api": `http://localhost:${backendPort}`,
+      "/api": `https://be10x.onrender.com`,
     },
   },
 });
