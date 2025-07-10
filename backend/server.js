83ad7c39c0ba1ed0bf06ab20a0c5ecc16b003266
maity03 <biman123maity@gmail.com>
2025-07-10 13:50:27 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
@@ -26,6 +26,4 @@ app.get("/", (req, res) => {
   res.send("Hello i am working");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
